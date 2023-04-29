7e84dc917702b80450650f4faeb0fc8fe88367e6
oetyng <oetyng@gmail.com>
2023-04-29 23:35:01 +0200

    fix: remove more mut access in node msg handling
    - This should decrease the cases of sequential access or deadlocks.

--- safenode/src/node/api.rs
+++ safenode/src/node/api.rs
@@ -95,7 +95,7 @@ impl Node {
             .await
             .map_err(|e| Error::CouldNotLoadWallet(e.to_string()))?;
 
-        let mut node = Self {
+        let node = Self {
             network: network.clone(),
             registers: RegisterStorage::new(root_dir),
             transfers: Transfers::new(root_dir, node_id, node_wallet),
@@ -125,9 +125,7 @@ impl Node {
         })
     }
 
-    // **** Private helpers *****
-
-    async fn handle_network_event(&mut self, event: NetworkEvent) -> Result<()> {
+    async fn handle_network_event(&self, event: NetworkEvent) -> Result<()> {
         match event {
             NetworkEvent::RequestReceived { req, channel } => {
                 self.handle_request(req, channel).await?
@@ -168,11 +166,7 @@ impl Node {
         Ok(())
     }
 
-    async fn handle_request(
-        &mut self,
-        request: Request,
-        response_channel: MsgResponder,
-    ) -> Result<()> {
+    async fn handle_request(&self, request: Request, response_channel: MsgResponder) -> Result<()> {
         trace!("Handling request: {request:?}");
         let response = match request {
             Request::Cmd(cmd) => Response::Cmd(self.handle_cmd(cmd).await),
@@ -207,7 +201,7 @@ impl Node {
         Ok(())
     }
 
-    async fn handle_query(&mut self, query: Query) -> QueryResponse {
+    async fn handle_query(&self, query: Query) -> QueryResponse {
         match query {
             Query::Register(query) => self.registers.read(&query, User::Anyone).await,
             Query::GetChunk(address) => {
